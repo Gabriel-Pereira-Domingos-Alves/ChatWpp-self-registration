d2cf3fccf4d4e14fb36e1d4b1780cc0c8272c4a6
Gabriel Domingos <gabriel.pereira.domingos.alves@gmail.com>
2024-08-20 22:27:48 -0300

    version 0.2

--- api-wpp/src/whatsapp/wppMessages.service.ts
+++ api-wpp/src/whatsapp/wppMessages.service.ts
@@ -39,7 +39,9 @@ export class MessagesService {
         } else {
             // Se o usuário já tem um estado salvo, segue o fluxo de conversação
             if (userState.stage === 'confirmingName') {
-                if (['sim', 's', 'yes', 'y'].includes(message.content.toLowerCase())) {
+                const confirmationRegex = /\b(sim|s|yes|y|ok|certo|correto)\b/i;
+                const confirmationRegex2 = /\b(não|n|no|nao)\b/i;
+                if (confirmationRegex.test(message.content.toLowerCase())){
                     await client.sendText(message.from, 'Por favor, informe seu e-mail.');
                     await this.prisma.userState.update({
                         where: { userId: message.from },
@@ -47,12 +49,14 @@ export class MessagesService {
                             name: message.sender.pushname || 'usuário'  // Atualizando o nome
                          },
                     });
-                } else {
+                } else if (confirmationRegex2.test(message.content.toLowerCase())) { 
                     await client.sendText(message.from, 'Por favor, informe o nome correto.');
                     await this.prisma.userState.update({
                         where: { userId: message.from },
                         data: { stage: 'collectingName' },
                     });
+                } else {
+                    await client.sendText(message.from, 'Não entendi. Por favor, responda com sim ou com não.');
                 }
             } else if (userState.stage === 'collectingName') {
                 await this.prisma.userState.update({
@@ -69,8 +73,17 @@ export class MessagesService {
                 await client.sendFile(
                     message.from,
                     'C:\\Users\\Fred\\Documents\\GitHub\\ChatWpp-self-registration\\api-wpp\\src\\whatsapp\\assets\\ebook-manutencao_aegs.pdf',
+                    //'/Users/gabrielalves/Documents/integra/ChatWpp-self-registration/api-wpp/src/whatsapp/assets/ebook-manutencao_aegs.pdf',
                     'ebook-manutencao_aegs',
                     'Ebook de manutenção de AEGs!'
+                );
+                await new Promise(resolve => setTimeout(resolve, 90000));
+                await client.sendText(message.from,
+                    'Já que você está interessado em manutenção de airsoft, dê uma olhada neste post que preparamos com todo o cuidado: https://airsoftnews.com.br/desvendando-os-segredos-do-rifle-de-airsoft-um-guia-completo-para-operadores-experientes/?utm_source=whatsapp-cta. Nele, você encontrará uma análise detalhada de todas as peças de uma arma de airsoft. Não perca essa oportunidade de aprofundar ainda mais seu conhecimento!'
+                );
+                await new Promise(resolve => setTimeout(resolve, 300000));
+                    await client.sendText(message.from,
+                    'Se você está interessado em receber informações exclusivas, atualizações e dicas valiosas sobre airsoft, junte-se ao nosso grupo de divulgação no WhatsApp! Lá, compartilhamos as novidades mais quentes e você pode se conectar com outros entusiastas do esporte. Não perca a oportunidade de estar sempre por dentro do que está acontecendo. É rápido, fácil e, o melhor de tudo, você vai adorar fazer parte dessa comunidade. Clique no link e entre para o nosso grupo agora: https://chat.whatsapp.com/JtB6wX8aHddDALGzEFgkOg. Estamos esperando por você!'
                 );
             }
         }
